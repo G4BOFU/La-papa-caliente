948f594e8dc6b7ca6d8a83388422adea727ae715
JRVS29 <106562402+JRVS29@users.noreply.github.com>
2022-06-02 22:10:59 -0400

    Update lista.java

--- lista.java
+++ lista.java
@@ -1,21 +1,14 @@
 package la.papa.caliente;
 
 import java.io.*;
-import java.util.Scanner;
 
 class Lista {
-    Scanner teclado = new Scanner(System.in);
-    nodo anterior;
-    int pase = 1;
-    int turno = 0;
-    String perdedores;
-    String ganador = null;
-    int modo;
-    int a;
 
     
 
-    nodo p, u, aux;
+    nodo p, u, aux, anterior;
+    nodo aux2  = aux;
+    int turno = 0;
     
     
     void add(String nombre){
@@ -39,57 +32,60 @@ class Lista {
     
     
 
-    nodo recorrer(int a, boolean horario) {
-        System.out.println("**ingrese cantidad de turnos igual a la cantidad de participantes**");
-        a = teclado.nextInt();
-        while (pase <= a){
-        int n = (int)(Math.random()*10+1);
-        System.out.println("juegue en modo horario (1) o anti-horario (2)");
-        modo = teclado.nextInt();
+    nodo recorrer(int n, boolean horario) {
         aux = p;
-        turno++;
-        pase++;
-        aux = horario ? aux.sig: aux.ant;
-        switch(modo){
-        case 1:
-            horario = true;
+        for (int i = 1; i < n; i++) {
+            
+            aux = horario ? aux.sig : aux.ant;
+
+        }
+        return aux;
+         
+        
+
+    }
+    
+    void modhorario(){
+    
+    aux = p;   
+    int n = (int)(Math.random()*10+1);
         for(int A = 0; A <= n; A++){
             anterior = aux;
-            aux = horario ? aux.sig : aux.ant;   
+            aux = aux.sig;
         if(A == n){
             System.out.println("caliente");
             anterior.sig = aux.sig;
             aux.sig.ant = anterior;
-            ganador = aux.dato;
             System.out.println("**"+aux.dato+" salio en el turno "+turno+"**");
+        
         }else{
             System.out.println("papa");
         }
-    }break;
-      case 2:
-          horario = false;
-       for(int y = 0; y <= n; y++){
+    
+    
+        }
+    }
+    
+    void modantihorario(){
+    int b = (int)(Math.random()*10+1);
+       aux = u;
+       for(int y = 0; y <= b; y++){
         anterior = aux;
-        aux = horario ? aux.sig : aux.ant;
-       if(y == n){
+        aux = aux.ant;
+       if(y == b){
            System.out.println("caliente");
            anterior.ant =aux.ant;
            aux.ant.sig = anterior;
-           ganador = aux.dato;
            System.out.println("**"+aux.dato+" Salio en el turno "+turno+"**");
+           
        }else{
            System.out.println("papa");
        }
            
-    }break;
-       
     }
-   
-         
-        
-
-     }
-        return aux;
+    
+    
+    
     }
     
 
@@ -122,6 +118,5 @@ class Lista {
 
 
    
-
 
 
